6ecf4a743044aeecb867eb790d8e7d6a9ba32ef1
Seungwon Park <wonvely941@gmail.com>
2024-08-14 19:42:33 +0900

    fix reactive layout

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -80,7 +80,7 @@ function App() {
   return (
     <div className="background">
       <div className="main-container">
-        <div className="game-title"> 추억의 가위바위보 게임 👉 ✊ 🖐️</div>
+        <div className="game-title"> 추억의 가위바위보 게임 <span class="title-icon">👉 ✊ 🖐️</span></div>
         <div className="result-container">
           <div className="result-wrapper">
             <div className="point-area">
